e74cda0f81ef9d36d4d915d2ee36f1a45e0a535b
Jakob den Brok <jdenbrok@student.ethz.ch>
2020-10-30 12:12:34 +0100

    Update processing_spec.py

--- PyStructure/processing_spec.py
+++ PyStructure/processing_spec.py
@@ -57,10 +57,11 @@ def process_spectra(sources_data, lines_data,fname,shuff_axis, run_success,just_
         low_mask = np.array(co21 > 2 * rms, dtype = int)
 
         mask = mask & (np.roll(mask, 1,1) | np.roll(mask,-1,1))
+
         #remove spikes:
-        mask = np.array((mask + np.roll(mask, 1, 0) + np.roll(mask, -1, 0))>=1, dtype = int)
-        low_mask = np.array((low_mask + np.roll(mask, 1, 0) + np.roll(low_mask, -1, 0))>=1, dtype = int)
-        
+        mask = np.array((mask + np.roll(mask, 1, 0) + np.roll(mask, -1, 0))>=3, dtype = int)
+        low_mask = np.array((low_mask + np.roll(mask, 1, 0) + np.roll(low_mask, -1, 0))>=3, dtype = int)
+
         #expand to cover all > 2sigma that have a 2-at-4sigma core
         for kk in range(5):
             mask = np.array(((mask + np.roll(mask, 1, 0) + np.roll(mask, -1, 0)) >= 1), dtype = int)*low_mask
