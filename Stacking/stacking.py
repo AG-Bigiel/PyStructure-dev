3d34192d7d7a4a01c9d109303ffe0a4fee85a123
Jakob den Brok <jdenbrok@student.ethz.ch>
2020-10-30 22:20:51 +0100

    Update stacking.py
    small fix, improve rms calculation

--- Stacking/stacking.py
+++ Stacking/stacking.py
@@ -58,16 +58,16 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
     xmin=bin_values["VAL"][flag_index[0]]
     xmax=bin_values["VAL"][flag_index[1]]
     binsize = bin_values["VAL"][flag_index[2]]
-    
+
     # -----------------------------------------------------------------------
     # RESTORE THE DATA STRUCTURES
     # -----------------------------------------------------------------------
     # Measurements
     #dir='/vol/alcina/data1/jdenbrok/Proj_I_2019/ngc_5194_database/measurements/'
-    
+
     dir = dir_data
     file = dir + galaxy + naming_convention
-    
+
     is_IDL = False
     if ".idl" in naming_convention:
         struct = idlsave.read(file, verbose=False, python_dict=True)
@@ -112,50 +112,50 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
             xvec = np.loadtxt(path_sigtir)/this_data["INT_VAL_12CO21"]
         elif xtype == "angle":
             xvec = this_data["theta_rad"]
-        
+
         ra = this_data["ra_deg"]
         decl = this_data["dec_deg"]
-        
-            
+
+
         # -----------------------------------------------------------------------
         # AVERAGE PIXEL MEASUREMENTS
         # -----------------------------------------------------------------------
 
         #collect all pixels with xval value
         stack = func.stack_pix_by_x(this_data,lines, xtype, xvec ,xmin, xmax , galaxy, binsize , median  = 'mean' ) #0= mean, 1=median
-        
+
         #--------------------------------------------------------------------------
         # STACK THE SPECTRA
         # -------------------------------------------------------------------------
 
         # stack spectra in those pixels
         shuffled_specs = {}
-        
+
         #length of the shuffeled spectrum
         nvaxis = len(this_data["SPEC_VAL_SHUFF"+prior_lines[0]][0])
         for line in lines+prior_lines:
             shuffled_specs["SPEC_VAL_SHUFF"+line] = this_data["SPEC_VAL_SHUFF"+line]
-            
-        
+
+
         # create a mask and combine for co21 and co10 at least:
         spec_co21 = shuffled_specs["SPEC_VAL_SHUFF"+prior_lines[0]]
-        
+
 
         idnans_co21 = np.ones(len(spec_co21))
-       
+
         for i in range(len(idnans_co21)):
             if not sum(np.array(np.isnan(spec_co21[i]), dtype = int)) == nvaxis:
                 idnans_co21[i] = 0
-            
+
         comb_mask = np.array(idnans_co21, dtype=int)
 
         for i in range(len(comb_mask)):
             if comb_mask[i] == 1:
                 spec_co21[i] = np.zeros(nvaxis)*np.nan
-               
+
         shuffled_specs[prior_lines[0]+"_spec_K"]  = spec_co21
-        
-        
+
+
         if do_smooth == False:
             if is_IDL:
                 vaxis = this_data[0]["SPEC_VCHAN0_SHUFF"+prior_lines[0]] + this_data[0]["SPEC_DELTAV_SHUFF"+prior_lines[0]] * np.arange(len(this_data[0]["SPEC_VAL_"+prior_lines[0]]))
@@ -165,7 +165,7 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
 
             stack_spec = stck_spc.stack_spec(co21_spec, xvec,xtype, xmin, xmax, binsize)
             stack[prior_lines[0]+"_spec_K"] = stack_spec["spec"]
-            
+
             # Iterate over the different lines that need to be stacked
 
             for line in lines:
@@ -173,7 +173,7 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
                 stack_spec = stck_spc.stack_spec(spec, xvec,xtype, xmin, xmax, binsize)
                 stack[line+"_spec_K"] = stack_spec["spec"]
 
-              
+
                 #print(np.shape(stack_spec["spec"]))
 
         # save some more galaxy parameters
@@ -195,7 +195,7 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
                           np.cos(np.pi/180*stack["incl_deg"])*stack["ncounts"]
 
 
-    
+
     #--------------------------------------------------------------------------
     # Fit Gaussian to spectral line
     # -------------------------------------------------------------------------
@@ -203,7 +203,7 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
     # Prepare the output of the fittinbf
     n_stacks = len(stack['xmid'])
     stack["prior_mask"] = np.zeros((nvaxis,n_stacks))*np.nan
-    
+
     for line in lines+prior_lines:
         stack["rms_K_"+line]       = np.zeros(n_stacks)*np.nan
         stack["peak_K_"+line]      = np.zeros(n_stacks)*np.nan
@@ -212,28 +212,28 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
         stack["limit_K_kms_"+line] = np.zeros(n_stacks)*np.nan
         stack["SNR_"+line] = np.zeros(n_stacks)*np.nan
 
-    
+
     for j in range(len(stack['xmid'])):
 
        #--------------------------------------------------------------------------
        # Need to first find the bounds using a prior (the brightest line)
        # -------------------------------------------------------------------------
         v = stack["vaxis_kms"]
-        
-        
-        
+
+
+
         # The user can give several priors. Iterate over the priors and check which one shows the stongest detection. Use that line as prior for that given bin.
         prior_specs = []
         masks = []
         rms_values = []
-        
+
         sn_low = sn_limits[0]
         sn_up = sn_limits[1]
         for prior_line in prior_lines:
-            
+
             prior = copy.copy(stack[prior_line+"_spec_K"][:,j])
-            
-            
+
+
             #Ignore the boundaries
             min_v = np.nanmin(vaxis[np.where(np.isfinite(prior))])
             max_v = np.nanmax(vaxis[np.where(np.isfinite(prior))])
@@ -241,25 +241,25 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
                               np.array(vaxis > np.max([(max_v - pad_v),0]), dtype = int))
             always_spec = always_empty | np.array(np.isfinite(prior)==0)
             prior[np.where(always_spec)]=np.nan
-            
-            
+
+
             #Estimate rms
             rms = median_absolute_deviation(prior, axis = None,ignore_nan=True)
             rms = median_absolute_deviation(prior[np.where(prior<3*rms)],ignore_nan=True)
-            
-            
+
+
             # Mask each spectrum
             mask = np.array(prior > sn_up * rms, dtype = int)
             low_mask = np.array(prior > sn_low * rms, dtype = int)
-       
+
             mask = mask & (np.roll(mask, 1) | np.roll(mask,-1))
-        
+
             #expand to cover all > 2sigma that have a 2-at-4sigma core
             for kk in range(10):
                 mask = np.array(((mask + np.roll(mask, 1) + np.roll(mask, -1)) >= 1), dtype = int)*low_mask
 
             mask = np.array((mask + np.roll(mask, 1) + np.roll(mask, -1))>=1, dtype = int)
-            
+
             prior_specs.append(stack[prior_line+"_spec_K"][:,j])
             masks.append(mask)
             rms_values.append(rms)
@@ -274,53 +274,53 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
             if prior_ii_comp>prior_ii_ref:
                 mask = masks[n+1]
                 rms = rms_values[n+1]
-        
+
         stack["prior_mask"][:,j] = mask
-        
+
         #line_vmean = np.nansum(v * prior*mask)/ np.nansum(prior*mask)
-        
+
         #prior_ii = np.nansum(prior*mask)*abs(v[1]-v[0])
-        
+
         #prior_uc = max([1, np.sqrt(np.nansum(mask))])*rms*abs(v[1]-v[0])
-        
+
         #stack["rms_K_"+prior_line][j] = rms
         #stack["peak_K_"+prior_line][j] = np.nanmax(prior)
         #stack["ii_K_kms_"+prior_line][j] = prior_ii
         #stack["uc_ii_K_kms_"+prior_line][j] = prior_uc
-        
-        
+
+
         """
         ToDo: If none of the priors shows a detection, the program should give back only an upper limit
         """
         for line in lines+prior_lines:
-            
-            
+
+
             # Fit the line
             spec_to_integrate = stack[line+"_spec_K"][:,j]
 
             spec_to_integrate[spec_to_integrate == 0] = np.nan
-            
+
             #calculate rms of line
             rms_line = median_absolute_deviation(spec_to_integrate, axis = None,ignore_nan=True)
-            rms_line = median_absolute_deviation(spec_to_integrate[np.where(prior<3*rms_line)],ignore_nan=True)
-            
+            rms_line = median_absolute_deviation(spec_to_integrate[np.where(spec_to_integrate<3*rms_line)],ignore_nan=True)
+
             line_ii = np.nansum(spec_to_integrate*mask)*abs(v[1]-v[0])
             line_uc = max([1, np.sqrt(np.nansum(mask))])*rms_line*abs(v[1]-v[0])
 
             # Fill in dictionary
             stack["rms_K_"+line][j] = rms_line
             stack["peak_K_"+line][j] = np.nanmax(line_ii)
-       
+
             stack["ii_K_kms_"+line][j] = line_ii
             stack["uc_ii_K_kms_"+line][j] = line_uc
-            
+
             SNR_line = line_ii/line_uc
             stack["SNR_"+line][j] = SNR_line
             if SNR_line<3:
                 #if window found: integrate over that
                 if np.nansum(mask)>2:
                     stack["limit_K_kms_"+line][j] = 2*rms*max([1, np.sqrt(np.nansum(mask))])*abs(v[1]-v[0])
-                
+
                 #if no window found: use (default) 30 km/s window
                 else:
                     stack["limit_K_kms_"+line][j] = 2*rms*no_detec_wdw
@@ -331,11 +331,11 @@ def get_stack(galaxy,prior_lines,lines, dir_save, dir_data ='./../../data/Databa
     print("[INFO]\t Successfull Finished")
 
     return stack
-    
-    
-   
-   
 
+
+
+
+"""
 # Specify the directories:
 final_direc = "./Example/Example_Results/"
 data_direc = "./../PyStructure/Output/"
@@ -358,3 +358,4 @@ prior = ["12CO21"]
 for xtype in xtypes:
     for galaxy in galaxies:
         res = get_stack(galaxy,prior,lines, final_direc, dir_data = data_direc, xtype = xtype, naming_convention="_data_struct_2020_10_02.npy")
+"""
