672375ce88b97abcfb971527061ac39043477efe
jdenbrok <56962565+jdenbrok@users.noreply.github.com>
2021-10-18 07:42:25 +0200

    Update create_database.py

--- PyStructure/create_database.py
+++ PyStructure/create_database.py
@@ -26,7 +26,7 @@ MODIFICATION HISTORY
 __author__ = "J. den Brok"
 __version__ = "v1.0.1"
 __email__ = "jdenbrok@astro.uni-bonn.de"
-__credits__ = ["A. Leroy ", "I. Beslic"]
+__credits__ = ["M. Jimenez-Donaire", "A. Leroy ", "I. Beslic"]
 
 
 import numpy as np
