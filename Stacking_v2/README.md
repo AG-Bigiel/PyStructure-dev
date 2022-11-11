bf79470be3d1175b08092fee5d3d53897a9539e5
jdenbrok <56962565+jdenbrok@users.noreply.github.com>
2022-11-11 11:04:13 +0100

    Upgrade stacking code to v2

---
+++ Stacking_v2/README.md
@@ -0,0 +1,25 @@
+# Stacking Code (Upgraded Version)
+
+The stacking code allows a user to stack spectra of a cube with respect to a predefined quantity, such as the galactocentric radius, star formation rate density and more. Here we give a short description and step-by-step guide on how to use the code for an optimal experience. 
+
+* The main file is the `stacking.py` file. 
+* The `how_to_stack.ipynb` is an Jupyter notebook with an example of how to run the code
+* The folder `Example` contains an `IDL` structure with the data to stack as well as the results of the example run
+
+## Updated (Still work in Progress)
+    * Can use weights
+    * Can perform weighting by SNR^2 
+    * remove the use of the bin_values file
+    
+## General Structure of the Code
+The `stacking.py` file contains the main part of the code which call upon all the other side files. The procdure is the following:
+
+1. It is specified by which quantity will be stacked (by the `xtype` variable).
+2. The to be stacked quantity is binned acording to the values given in `bin_values.txt` using the subscript `stacking_func.py`.
+3. The spectra are stacked for each bin using the functions in `stack_specs.py`.
+4. We fit a Gaussian to the stacked spectra to get better constraints of the intesnity of the line. For this the fitting procedure in `fitting_routine.py` is used.
+
+## Tutorial
+In order to provide an optimal experience with the code, have a look at the `how_to_stack.ipynb`  notebook. In there is a step--by--step guide on what to do to get your spectra stacked.
+
+
---
+++ Stacking_v2/scripts/stack_specs.py
@@ -0,0 +1,127 @@
+import numpy as np
+from astropy.stats import median_absolute_deviation
+import matplotlib.pyplot as plt
+
+
+
+def stack_spec(spec_ra,
+                x, \
+                xtype, \
+                xmin_in = None, \
+                xmax_in = None, \
+                binsize_in = None,\
+                irregular = False,\
+                mask =None,\
+                use_median = False,\
+                weights = None):
+
+    # Number of channels
+    n_vaxis = len(spec_ra[0])
+    # Number of lines of sight
+    n_vec = len(spec_ra)
+    spec_array = np.zeros((n_vec, n_vaxis))
+    for i in range(n_vec):
+        spec_array[i, :] = spec_ra[i]
+    spec_ra = spec_array
+    if mask is None:
+        mask = np.array(~np.isnan(x), dtype = int)
+
+    #--------------------------------------------------------------------------
+    # Construct the bins
+    #--------------------------------------------------------------------------
+
+    if xtype in ['sfr',"12co10","12co21","PACS", "sigtir", "TIR_co21", "TIR_co10"]:
+        xmax = xmax_in
+        xmin = xmin_in
+        deltax = (xmax - xmin)
+        if binsize_in is None:
+            #    ... DEFAULT TO 10 BINS
+            print("[WARNING]\t No binsize specified. Making 10 bins.")
+            binsize = deltax / 10.
+        else:
+            binsize = binsize_in
+    #    MAKE THE BINS
+        nbins = int(abs(np.ceil(deltax / binsize)))
+        xmin_bin = 10**(np.arange(nbins)*binsize)*10**xmin
+        xmax_bin = xmin_bin*10**binsize #< 10**xmax_in
+        xmid_bin = xmin_bin*10**(binsize*0.5)
+    else:
+        #WORK OUT THE MINIMUM FOR THE BINS
+        if xmin_in == None:
+            xmin = np.nanmin(x)
+        else:
+            xmin = xmin_in
+        if xmax_in == None:
+            xmax = np.nanmax(x)
+        else:
+            xmax = xmax_in
+        if not irregular:
+            deltax = (xmax - xmin)
+            if binsize_in is None:
+                #    ... DEFAULT TO 10 BINS
+                print("[WARNING]\t No binsize specified. Making 10 bins.")
+                binsize = deltax / 10.
+            else:
+                binsize = binsize_in
+            #;    MAKE THE BINS
+            nbins = int(abs(np.ceil(deltax / binsize)))
+            xmin_bin = np.arange(nbins)*binsize+xmin
+            xmax_bin = xmin_bin+binsize #< xmax
+            xmid_bin = (xmin_bin+xmax_bin)*0.5
+
+
+        else:
+            nbins = len(xmax_in)
+            xmin_bin = xmin_in
+            xmax_bin = xmax_bin
+            xmid_bin = (xmin_bin+xmax_bin)*0.5
+
+    #--------------------------------------------------------------------------
+    # Initializze the output
+    #--------------------------------------------------------------------------
+    stacked_spec = np.zeros((n_vaxis, nbins))*np.nan
+    counts = np.zeros((n_vaxis, nbins))
+
+
+    #--------------------------------------------------------------------------
+    # Loop over bins
+    #--------------------------------------------------------------------------
+    
+    for i in range(nbins):	
+        
+        binind = np.where(np.logical_and(np.logical_and(x >= xmin_bin[i],x<xmax_bin[i]),\
+                          mask == 1))[0]
+        binct = len(binind)
+       
+        if binct == 0:
+            continue
+        elif binct == 1:
+            stacked_spec[:,i] = spec_ra[binind,:]
+            counts[:,i] = np.isfinite(spec_ra[binind,:])
+            continue
+        else:
+            
+            # Average together all the SPECTRA
+            image_here = spec_ra[binind,:]
+            counts[:,i] = np.sum(np.isfinite(spec_ra[binind,:]), axis = 0)
+            
+            if use_median:
+                stacked_spec[:,i] = np.nanmedian(image_here, axis = 0)
+              
+            else:
+                if weights is not None:
+                    stacked_spec[:,i] = np.nansum(weights[binind,np.newaxis]*image_here, axis = 0)/np.nansum(weights[binind])
+                else:
+                    stacked_spec[:,i] = np.nansum(image_here, axis = 0)/counts[:,i]
+                
+            
+    #--------------------------------------------------------------------------
+    # Return Stacked Spectrum
+    #--------------------------------------------------------------------------
+    output = {}
+    output["spec"] = stacked_spec
+
+    output["counts"] = counts
+    output["xmid"] = xmid_bin
+    
+    return output
